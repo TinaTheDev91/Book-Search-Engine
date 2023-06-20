31e7268d8a79edf41e063be83c6ce13e5b201985
Tina Croxton <122223572+TinaTheDev91@users.noreply.github.com>
2023-06-20 13:00:41 -0400

    wrote resolver, still need to add delete book mutation

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,56 @@
+const { AuthenticationError } = require('apollo-server-express')
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+
+const resolvers = {
+  Query: {
+    user: async (parent, { username, userId }) => {
+      return User.findOne({ $or: [{ _id: userId }, { username: username }] });
+    },
+  },
+  Mutation: {
+    createUser: async (parent, { username, email, password }) => {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+        return { token, user }
+    },
+    login: async (parent, { username, email, password }) => {
+      const user = await User.findOne({ $or: [{ username: username }, { email: email }] });
+        
+      if (!user) {
+        throw new AuthenticationError('Cannot locate this user');
+      }
+      if (!correctPw) {
+        throw new AuthenticationError('Wrong password!');
+      }
+
+      const token = signToken(user);
+
+      return { token, user }
+    },
+    saveBook: async (parent, args, context) => {
+       if (context.user) {
+        const book = await User.create(args);
+
+        await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $addToSet: { savedBooks: args } },
+            { new: true, runValidators: true }
+        );
+        return book;
+       }
+       throw new AuthenticationError('Please log in to perform this action')
+    },
+    deleteBook: async (parent, args, context) => {
+        if (context.user) {
+            const book = await User.findOneAndDelete(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: params.bookId } }}
+            )
+        }
+    }
+  },
+};
+
+module.exports = resolvers;
